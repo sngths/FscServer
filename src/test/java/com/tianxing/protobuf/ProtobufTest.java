9a047443ccb7879d2019921ba73b952b7da187a9
tianxing <1035273486@qq.com>
2017-03-06 19:55:08 +0800

    添加protobuf测试代码

---
+++ src/test/java/com/tianxing/protobuf/ProtobufTest.java
@@ -0,0 +1,19 @@
+package com.tianxing.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.tianxing.entity.Testentity;
+import org.junit.Test;
+
+/**
+ * Created by tianxing on 17/3/6.
+ */
+public class ProtobufTest {
+
+    @Test
+    public void test() throws InvalidProtocolBufferException {
+        Testentity.aaa aaa  = Testentity.aaa.newBuilder().setAasa("aaa").build();
+        Testentity.aaa aaa1 = Testentity.aaa.parseFrom(aaa.toByteArray());
+        System.out.println(aaa1.getAasa());
+    }
+
+}
